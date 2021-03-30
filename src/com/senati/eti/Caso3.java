4bcab6af87d6a33e67e88b11b690667fda340e95
usuario <usuario@DESKTOP-HC4CEB3>
2021-03-30 09:02:57 -0500

    terminado

--- src/com/senati/eti/Caso3.java
+++ src/com/senati/eti/Caso3.java
@@ -1,9 +1,36 @@
 package com.senati.eti;
+import java.util.Scanner;
 
 public class Caso3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+     Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Ingrese numero........:");
+		int n = sc.nextInt();
+
+			
+		String respuesta = "";
+		//factorial es el acomulador 
+		
+		int i = 1,factorial = 1;
+		if (n<=7)
+		 while (i <=n) {
+			factorial*= i; 
+			respuesta = "El factorial es "+factorial;
+			if (i<n)
+				System.out.print(i +"x");
+			else
+				System.out.println(i + "=" + factorial);
+			
+			i++	;}
+		else 
+			respuesta=("El numero es superior a 7  ");
+		
+	
+		
+		System.out.println("" + respuesta);
+		
 
 	}
 
